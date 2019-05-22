701c48d75f041cdcb28eaf57d94d61f002d17986
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-05-22 18:35:28 +0100

    styled hom page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,22 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :components]
+  skip_before_action :authenticate_user!, only: [:home, :components, :about_us, :how_it_works]
 
   def home
+    @shirts = Shirt.all
+    @fabrics = ['fabric-1.png', 'fabric-2.png', 'fabric-3.png', 'fabric-4.png', 'fabric-5.png', 'fabric-6.png', 'fabric-7.png', 'fabric-8.png', 'fabric-9.png', 'fabric-10.png',]
+    @num = 1
+
   end
 
   def components
     @shirts = Shirt.all
     @fabrics = ['fabric-1.png', 'fabric-2.png', 'fabric-3.png', 'fabric-4.png', 'fabric-5.png', 'fabric-6.png', 'fabric-7.png', 'fabric-8.png', 'fabric-9.png', 'fabric-10.png',]
     @num = 1
+  end
+
+  def about_us
+  end
+
+  def how_it_works
   end
 end
